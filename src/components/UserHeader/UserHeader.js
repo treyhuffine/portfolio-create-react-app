d8f61755baccde69e1bfc278e6be722d01ab83a9
Trey Huffine <treyhuffine@gmail.com>
2020-01-27 17:47:49 -0600

    View resume link

--- src/components/UserHeader/UserHeader.js
+++ src/components/UserHeader/UserHeader.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { ArrowRight16 } from '@carbon/icons-react';
 
-import { HeaderContainer, Header, Image } from './styles';
+import { HeaderContainer, Header, Image, ViewResumeLink } from './styles';
 
 const UserHeader = ({ user }) => {
   const location = useLocation();
@@ -14,11 +15,11 @@ const UserHeader = ({ user }) => {
           <h2>{user.basics.name}</h2>
           <h4>
             <a
-              href={`https://gitconnected.com/${user.basics.usernameCaseSensitive}`}
+              href={`https://gitconnected.com/${user.basics.username}`}
               target="_blank"
               rel="noreferrer noopener"
             >
-              @{user.basics.usernameCaseSensitive}
+              @{user.basics.username}
             </a>
           </h4>
           <p>{user.basics.label}</p>
@@ -33,6 +34,16 @@ const UserHeader = ({ user }) => {
           </p>
         </div>
       </Header>
+      <div>
+        <ViewResumeLink
+          href={`https://gitconnected.com/${user.basics.username}/resume`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span>View Résumé</span>
+          <ArrowRight16 />
+        </ViewResumeLink>
+      </div>
     </HeaderContainer>
   );
 };
--- src/components/UserHeader/styles.js
+++ src/components/UserHeader/styles.js
@@ -1,10 +1,14 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+
   @media (max-width: 640px) {
-    display: ${({ isHome }) => (!isHome ? 'none' : 'block')};
+    display: ${({ isHome }) => (!isHome ? 'none' : 'flex')};
+    flex-direction: column;
   }
-`
+`;
 
 export const Header = styled.div`
   display: flex;
@@ -12,10 +16,31 @@ export const Header = styled.div`
   @media (max-width: 640px) {
     flex-direction: column;
   }
-`
+`;
 
 export const Image = styled.img`
   width: 200px;
   margin-right: 1rem;
   border-radius: 2px;
-`
+`;
+
+export const ViewResumeLink = styled.a`
+  display: flex;
+  text-decoration: none;
+  padding: 0.75rem 1.5rem;
+  font-weight: bold;
+  align-items: center;
+  margin-top: 1rem;
+  border: 2px solid #2ecc40;
+  background-color: rgba(46, 204, 64, 0.3);
+  transition: background-color 250ms ease;
+
+  &:hover {
+    background-color: #2ecc40;
+  }
+
+  svg {
+    fill: white;
+    margin-left: 8px;
+  }
+`;
